214ec984817a08aba72653ea5b5538f5a48b4d63
Hamir Mahal <hamirmahal@gmail.com>
2024-04-21 16:43:11 -0700

    reproduce formatting issue with mjs config file

---
+++ index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// object
+const obj = {
+    name: "Aisha",
+    age: 30,
+};
---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "none",
+};
+
+export default config;
